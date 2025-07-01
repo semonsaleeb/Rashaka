94fa1a108436fe2c228a1a679b6d2f34428c3969
unknown <semonsaleeb@gmail.com  git config --global user.name semongit config --global user.email semonsaleeb@gmail.com>
2025-07-01 16:12:08 +0300

    home hero 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { CommonModule } from '@angular/common';
+import { Hero } from './pages/home/hero/hero';
 
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, Header],
+  imports: [CommonModule, RouterOutlet, Header,Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
