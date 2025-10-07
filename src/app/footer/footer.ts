2bac2e727c897fc7ddd188d2c5aa1cfa100f143e
mahmoudabbas <mahmoud.abbas@mediazoneag.com>
2025-10-07 17:21:03 +0300

    error messege translation4

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -2,6 +2,7 @@ import { CommonModule, ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageService } from '../services/language.service';
 
 @Component({
   selector: 'app-footer',
@@ -11,17 +12,35 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 })
 export class Footer {
 
-  constructor(private viewportScroller: ViewportScroller, public translate: TranslateService) {}
+  constructor( private languageService: LanguageService, private viewportScroller: ViewportScroller, public translate: TranslateService) {}
+
+   currentLang: string = 'ar';
+  dir: 'ltr' | 'rtl' = 'rtl';
+
+  ngOnInit(): void {
+;
+
+          // إعداد اللغة
+    this.currentLang = this.languageService.getCurrentLanguage();
+    this.dir = this.currentLang === 'ar' ? 'rtl' : 'ltr';
+
+    this.languageService.currentLang$.subscribe((lang) => {
+      this.currentLang = lang;
+      this.dir = lang === 'ar' ? 'rtl' : 'ltr';
+    });
+  
+  }
 
 scrollToTop() {
   this.viewportScroller.scrollToPosition([0, 0]);
 }
-formatText(text: string): string {
-  if (!text) return '';
-  const words = text.split(' ');
-  return words.map((word, i) =>
-    (i + 1) % 5 === 0 ? word + '<br>' : word
-  ).join(' ');
-}
+// formatText(text: string): string {
+//   if (!text) return '';
+//   const words = text.split(' ');
+//   return words.map((word, i) =>
+//     (i + 1) % 5 === 0 ? word + '<br>' : word
+//   ).join(' ');
+// }
+
 
 }
